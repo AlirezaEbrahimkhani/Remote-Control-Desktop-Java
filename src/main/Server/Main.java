fb4301cbcc28e678b77e6bb3df46d3a88e00b3de
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-12-27 23:18:35 +0330

    Main Class of Server side implemented

---
+++ src/main/Server/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  public static void main(String[] args) {
+    SetPassword frame1 = new SetPassword();
+    frame1.setSize(300, 80);
+    frame1.setLocation(500, 300);
+    frame1.setVisible(true);
+  }
+}
