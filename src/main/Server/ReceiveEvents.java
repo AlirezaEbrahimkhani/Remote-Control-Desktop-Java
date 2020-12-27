e491677e490e9b9b4dc47ed0798af092f71c6574
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-12-27 23:14:40 +0330

    create ReceiveEvents Class

---
+++ src/main/Server/ReceiveEvents.java
@@ -0,0 +1,8 @@
+import java.awt.Robot;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/* Used to recieve server commands then execute them at the client side*/
+
+class ReceiveEvents extends Thread {}
