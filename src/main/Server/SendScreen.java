ccd37b152b3ef3345ee18cf53c0565920a51981a
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-12-27 23:15:59 +0330

    Create Send Screen Class

---
+++ src/main/Server/SendScreen.java
@@ -0,0 +1,9 @@
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import javax.imageio.ImageIO;
+
+class SendScreen extends Thread {}
