ef64ef6a51f2a266f513322e97f5e36fbfd2a4af
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-12-27 23:16:38 +0330

    Capture Screen and Send to Client

--- src/main/Server/SendScreen.java
+++ src/main/Server/SendScreen.java
@@ -6,4 +6,43 @@ import java.io.OutputStream;
 import java.net.Socket;
 import javax.imageio.ImageIO;
 
-class SendScreen extends Thread {}
+class SendScreen extends Thread {
+
+  Socket socket = null;
+  Robot robot = null;
+  Rectangle rectangle = null;
+  boolean continueLoop = true;
+
+  OutputStream oos = null;
+
+  public SendScreen(Socket socket, Robot robot, Rectangle rect) {
+    this.socket = socket;
+    this.robot = robot;
+    rectangle = rect;
+    start();
+  }
+
+  public void run() {
+    try {
+      oos = socket.getOutputStream();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+
+    while (continueLoop) {
+      BufferedImage image = robot.createScreenCapture(rectangle);
+
+      try {
+        ImageIO.write(image, "jpeg", oos);
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
